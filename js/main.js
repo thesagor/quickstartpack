1582b9680ba8ca37fd847fd88a1880093684665a
iamsagorahmed <nilsagor004@gmail.com>
2017-04-06 21:19:30 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+  $(".slider").owlCarousel({
+    navigation : true,
+    autoPlay:true,
+    pagination:true,
+    items: 3,
+    loop:true,
+    
+  });
+ 
+});
